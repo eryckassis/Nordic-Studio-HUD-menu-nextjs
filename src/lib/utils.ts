8df20a703399571441bd544a73fa9d9e79ccfd69
Eryck Assis <eng.assis.dev@gmail.com>
2025-11-30 21:45:02 -0300

    Add: integrate clsx and tailwind-merge for utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,8 @@
 // General utility functions
+
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
